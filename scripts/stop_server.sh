197c0f8dcfb8d089458a2e533abcdcbd0774ced8
Jerry Britto <jbritto2004@gmail.com>
2025-02-15 15:15:07 +0530

    changed stop script

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,11 +2,7 @@
 
 echo "Checking for running Node.js servers..."
 
-# Find all running Node.js processes
-if pgrep node > /dev/null; then
-    echo "Stopping all running Node.js servers..."
-    pkill -f "node index.js"
-    echo "All Node.js servers have been stopped."
-else
-    echo "No Node.js servers are currently running."
-fi
+echo "Stopping all running Node.js servers..."
+pkill -f "node index.js"
+echo "All Node.js servers have been stopped."
+
